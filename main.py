0a69fbf76b2542daa93062f7a9ba26ac8dc8d8cb
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-07-14 10:00:16 +0900

    設定ファイルの追加

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 
 from jinja2 import Environment, FileSystemLoader
+import yaml
 
+
+with open(os.path.join(os.path.dirname(__file__), "zrc-builder.yml")) as r:
+    cfg = yaml.load(r)
 
 loader = FileSystemLoader(os.path.dirname(__file__))
 env = Environment(loader=loader)
 
-for k in ["zprofile", "zshrc", "zshenv"]:
-    template = env.get_template(k)
-    with open(os.path.join(os.path.dirname(__file__), ".{}".format(k)), "w") as w:
+for src, item in cfg["entry"].items():
+    template = env.get_template(src)
+    with open(os.path.join(os.path.dirname(__file__), item["dest"]), "w") as w:
         w.write(template.render())
